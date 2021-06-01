3cb0c5eeb2de1ef0355753ee9a0332632a27390e
Md Ismail Hossain <ismailtex043@gmail.com>
2021-06-02 00:33:49 +0600

    seassion

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Loading = (props) => {
+  return (
+    <div class="ui active dimmer">
+      <div class="ui big text loader">{props.message}</div>
+    </div>
+  );
+};
+Loading.defaultProps = {
+  message: "loading...",
+};
+export default Loading;
